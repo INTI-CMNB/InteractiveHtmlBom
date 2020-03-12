65fdb81e203d6122ef824913a2b80aaad827ccf6
Salvador E. Tropea <salvador@inti.gob.ar>
2020-03-12 11:14:35 -0300

    Debian packaging files.

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import io
+import os
+import sys
+import glob
+import os.path
+from distutils.core import setup
+
+# Package meta-data.
+NAME = 'InteractiveHtmlBom'
+NAME_PKG = NAME
+DESCRIPTION = 'Interactive HTML BOM generation plugin for KiCad'
+URL = 'https://github.com/INTI-CMNB/InteractiveHtmlBom/'
+EMAIL = 'unknown'
+AUTHOR = 'qu1ck'
+
+here = os.path.abspath(os.path.dirname(__file__))
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+     long_description = '\n' + f.read()
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(script_dir)
+os.environ['INTERACTIVE_HTML_BOM_CLI_MODE'] = 'True'
+import InteractiveHtmlBom.version
+
+setup(name=NAME,
+    version=InteractiveHtmlBom.version.LAST_TAG[1:],
+    description=DESCRIPTION,
+    long_description=long_description,
+    #long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=[NAME_PKG, NAME_PKG+'/core', NAME_PKG+'/ecad', NAME_PKG+'/ecad/kicad_extra', NAME_PKG+'/dialog'],
+    package_dir={NAME_PKG: NAME_PKG},
+    package_data={NAME_PKG: ['icon.png','web/*']},
+    #data_files=[('./', [NAME+'/icon.png']),
+    #            ('web', glob.glob(NAME+'/web/*'))],
+    scripts=['InteractiveHtmlBom/generate_interactive_bom.py'],
+    #install_requires=['wx'],
+    classifiers = ['Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+    ],
+    platforms   = 'POSIX',
+    license     = 'MIT'
+)
