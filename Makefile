e0a4b522c651cc69896d1f4ce974dab95d042595
Salvador E. Tropea <salvador@inti.gob.ar>
2020-06-16 13:47:06 -0300

    Added two very naive tests
    The main objetive is to determine if we really need to create a wx.App object.
    See 058ade94eb2934c4e1ec1d9d8ab54ff51d937f5b discussion.

--- Makefile
+++ Makefile
@@ -1,9 +1,22 @@
 #!/usr/bin/make
+PY_COV=python3-coverage
 
 deb:
 	fakeroot dpkg-buildpackage -uc -b
 
 deb_clean:
 	fakeroot debian/rules clean
+
+lint:
+	# flake8 --filename is broken
+	# stop the build if there are Python syntax errors or undefined names
+	flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+	flake8 . --count --statistics
+
+test:
+	$(PY_COV) erase
+	pytest-3
+	$(PY_COV) report
+
 
 .PHONY: deb deb_clean
---
+++ tests/tests/test_ibom.py
@@ -0,0 +1,59 @@
+"""
+Tests of InteractiveHtmlBom BoM files
+
+The bom.sch has R1, R2 and C1
+We test:
+- HTML
+
+For debug information use:
+pytest-3 --log-cli-level debug
+
+"""
+
+import os
+import sys
+# Look for the 'utils' module from where the script is running
+prev_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if prev_dir not in sys.path:
+    sys.path.insert(0, prev_dir)
+# Utils import
+from utils import context
+prev_dir = os.path.dirname(prev_dir)
+if prev_dir not in sys.path:
+    sys.path.insert(0, prev_dir)
+from kiplot.misc import (BOM_ERROR)
+
+BOM_DIR = 'BoM'
+
+
+def test_ibom():
+    prj = 'bom'
+    ctx = context.TestContext('Simple', prj, None, BOM_DIR)
+    ctx.run()
+    ctx.expect_out_file('ibom.html')
+    ctx.clean_up()
+
+def test_ibom_utf8_filename():
+    prj = 'ñandú'
+    ctx = context.TestContext('Simple', prj, None, BOM_DIR)
+    ctx.run(extra = ['--name-format', '%f_ibom'])
+    ctx.expect_out_file('ñandú_ibom.html')
+    ctx.clean_up()
+
+
+#     # Check all outputs are there
+#     # We us this format: %f_iBoM
+#     name = os.path.join(BOM_DIR, prj+'_iBoM')
+#     html = name+'.html'
+# def test_ibom_no_ops():
+#     prj = 'bom'
+#     ctx = context.TestContext('BoM_interactiveNoOps', prj, BOM_DIR)
+#     ctx.run()
+#     ctx.expect_out_file(os.path.join(BOM_DIR, 'ibom.html'))
+#     ctx.clean_up()
+# 
+# 
+# def test_ibom_fail():
+#     ctx = context.TestContext('BoM_interactiveFail', 'bom_no_xml', 'ibom', BOM_DIR)
+#     ctx.run(BOM_ERROR)
+#     ctx.clean_up()
