570abf2f74367a0202741e9a26e8d6f5cfd8da74
Salvador E. Tropea <salvador@inti.gob.ar>
2020-06-16 13:38:21 -0300

    Excluded Python 2 code from linter
    Just a line in InteractiveHtmlBom/ecad/easyeda.py that avoids running flake8.

--- InteractiveHtmlBom/ecad/easyeda.py
+++ InteractiveHtmlBom/ecad/easyeda.py
@@ -9,7 +9,7 @@ PY3 = sys.version_info[0] == 3
 if PY3:
     string_types = str
 else:
-    string_types = basestring
+    string_types = basestring  # noqa F821
 
 
 class EasyEdaParser(EcadParser):
